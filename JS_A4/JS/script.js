fda6f7aae5842f0a206fbc99b5aedfc0f3e3995b
Bhagyesh0718 <156474049+Bhagyesh0718@users.noreply.github.com>
2024-04-15 22:37:53 -0400

    Add files via upload

---
+++ JS_A4/JS/script.js
@@ -0,0 +1,39 @@
+const apiKey = "863ba49fd3574cd78f332016233110"; // API key
+        const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Barrie`; // For Barrie Location
+        
+        // Fetch weather data from API
+        fetch(apiUrl)
+            .then((response) => {
+                // Verify if the response is successful
+                if (!response.ok) {
+                    throw new Error("The network did not respond satisfactorily.");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Choose all elements that have the class weatherInfo
+                const weatherInfoElements =
+                    document.querySelectorAll(".weatherInfo");
+
+                // Gather the important details from the JSON response.
+                const location = data.location.name;
+                const temperature = data.current.temp_c;
+                const weatherDescription = data.current.condition.text;
+
+                // data template to be displayed
+                const weatherData = `
+              <div class="weather-output">
+                <h2>Current Weather in ${location}</h2>
+                <p>Temperature: ${temperature}°C</p>
+                <p>Condition: ${weatherDescription}</p>
+                <pre><code>${JSON.stringify(data, null, 2)}</code></pre>
+              </div>
+            `;
+
+                weatherInfoElements.forEach((element) => {
+                    element.innerHTML = weatherData;
+                });
+            })
+            .catch((error) => {
+                console.error("An issue occurred while retrieving or interpreting the weather data.", error);
+            });
